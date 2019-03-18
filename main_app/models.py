dfb3c26108ecae30b79b530701e3b8262f891e3c
jjoll <jjoll@me.com>
2019-03-18 17:13:07 -0500

    s

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 RATING = (
@@ -16,16 +16,14 @@ RATING = (
 class Project(models.Model):
     cohort_date = models.DateField('project date')
     name = models.CharField(max_length = 100)
-    image = models.CharField(max_length = 200)
     description = models.TextField(max_length = 250)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
     role = models.TextField(max_length = 100)
-    technology = models.ForeignKey(Technology, on_delete = models.CASCADE)
     feedback = models.TextField(max_length = 250)
     git_hub_link = models.CharField(max_length = 250)
     deployed_app_link = models.CharField(max_length = 250)
 
-    
+
     def __str__(self):
         return self.name
 
@@ -36,8 +34,8 @@ class Project(models.Model):
         return f"Image for project_id: {self.project_id} @{self.url}"
 
     # change the default sort
-    class Meta:
-        ordering = ['-date']
+    # class Meta:
+    #     ordering = ['-date']
 
 class Technology(models.Model):
     tech_type = models.TextField(max_length = 250)
@@ -48,7 +46,6 @@ class Technology(models.Model):
 class Review(models.Model):
     review = models.TextField(max_length = 250)
     rating = models.IntegerField(
-    max_length = 1,
     choices = RATING,
     )
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
@@ -64,7 +61,7 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'image_id': self.id})
-    
+
 class User(models.Model):
     first = models.CharField(max_length = 100)
     last = models.CharField(max_length = 100)
@@ -72,7 +69,6 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     spetialty = models.CharField(max_length = 100)
     cohort_date = models.DateField('project date')
-    projects = models.ManyToManyField(Project)
     git_hub_link = models.CharField(max_length = 250)
     linkedin_link = models.CharField(max_length = 250)
     deployed_app_link = models.CharField(max_length = 250)
