e4ee2d255b1bd6807c57ebcc4757294aa4150655
AndreykaVSR <andreykasokolov@gmail.com>
2019-03-18 22:44:19 -0500

    commiting changes to be able to switch to Andrey branch

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 RATING = (
